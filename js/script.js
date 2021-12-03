5633d13754f7b4686aacc44debb840e9f5d4213e
Lolpawer <dadu.petrosino@gmail.com>
2021-12-03 15:44:12 +0100

    skeleton

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let outputHtml = document.getElementById('output');
+
+//prima chiedo all'utente il numero di chilometri che vuole percorrere
+//è l'età del passeggero
+
+//prima calcolo il prezzo base del biglietto, sapendo che costa €0,21 al km
+
+//poi controllo se devo applicare sconti in base all'età e, in caso, calcolo il totale
+
+//poi stampo il risultato
