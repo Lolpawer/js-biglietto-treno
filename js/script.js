abd88dc15867717f4b31bb65f3f82ba2e0f0cbe2
Lolpawer <dadu.petrosino@gmail.com>
2021-12-03 16:00:08 +0100

    prezzo biglietto

--- js/script.js
+++ js/script.js
@@ -3,8 +3,31 @@ let outputHtml = document.getElementById('output');
 //prima chiedo all'utente il numero di chilometri che vuole percorrere
 //è l'età del passeggero
 
+let chilometriUtente = prompt('Quanti chilometri deve percorrere il passeggero?');
+chilometriUtente = parseInt(chilometriUtente);
+let etàUtente = prompt('Quanti hanni ha il passeggero?');
+etàUtente = parseInt(etàUtente);
+
 //prima calcolo il prezzo base del biglietto, sapendo che costa €0,21 al km
+
+let prezzoDistanza = chilometriUtente * 0.21
 
 //poi controllo se devo applicare sconti in base all'età e, in caso, calcolo il totale
 
+let prezzoFinale;
+
+if (etàUtente < 18) {
+    prezzoFinale = prezzoDistanza - (prezzoDistanza * 20 / 100);
+}
+
+else if (etàUtente > 65) {
+    prezzoFinale = prezzoDistanza - (prezzoDistanza * 40 / 100);
+}
+
+else {
+    prezzoFinale = prezzoDistanza;
+}
+
 //poi stampo il risultato
+
+outputHtml.innerHTML += `Il tuo biglietto costa € ${prezzoFinale.toFixed(2)}.`
